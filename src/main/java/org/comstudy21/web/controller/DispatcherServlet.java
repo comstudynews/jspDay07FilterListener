227b85b26caf2c8ad657bfdea584811a094a2c72
origin <comstudynews@gmail.com>
2024-05-29 14:41:49 +0900

    First Commit day07

---
+++ src/main/java/org/comstudy21/web/controller/DispatcherServlet.java
@@ -0,0 +1,40 @@
+package org.comstudy21.web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	ServletContext context;
+	PrintWriter out;
+	HttpSession session;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		context = config.getServletContext();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		context.log(">>>> doGet - DispatcherServlet");
+
+		out = response.getWriter();
+
+		out.println("<h1>컴스터디 웹앱 실행</h1>");
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		context.log(">>>> doPost - DispatcherServlet");
+	}
+}
